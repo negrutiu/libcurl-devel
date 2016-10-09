8c80c7ea0719a4dc45af7fa7b71798bf101d4186
Marius Negruțiu <marius.negrutiu@gmail.com>
2016-10-09 12:25:39 +0300

    + Upgrade nghttp2

--- _Get_nghttp2.bat
+++ _Get_nghttp2.bat
@@ -10,9 +10,9 @@ cd /d "%~dp0"
 
 set LIBNAME=nghttp2
 set URL=https://github.com/nghttp2/nghttp2.git
-set BRANCH=v1.14.0
-set VER=1.14.0
-set VER_NUM=0x010e00
+set BRANCH=v1.15.0
+set VER=1.15.0
+set VER_NUM=0x010f00
 
 echo Working with "%BRANCH%" tag
 echo Verify if newer (stable) %LIBNAME% versions are available!
