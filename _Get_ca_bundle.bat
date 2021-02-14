80621fc238c65deb7d38cb67cbff3217ee19f1b8
Marius Negruțiu <marius.negrutiu@protonmail.com>
2021-02-14 08:17:25 +0200

    Fixed cacert.pem download URL

--- _Get_ca_bundle.bat
+++ _Get_ca_bundle.bat
@@ -1,5 +1,5 @@
 @REM PowerShell.exe -ExecutionPolicy Unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 ; Invoke-WebRequest -Uri https://curl.haxx.se/ca/cacert.pem -OutFile '%~dp0\cacert.pem'"
 
-curl.exe -v -z curl-ca-bundle.crt -o curl-ca-bundle.crt --no-progress-meter -w "HTTP %%{http_code}" https://curl.haxx.se/ca/cacert.pem
+curl.exe -v -z curl-ca-bundle.crt -o curl-ca-bundle.crt --no-progress-meter -w "HTTP %%{http_code}" https://curl.se/ca/cacert.pem
 if %errorlevel% neq 0 pause && exit /B %errorlevel%
 @REM pause
