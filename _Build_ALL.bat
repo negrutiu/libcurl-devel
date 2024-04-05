146233f4f271f1d3d6e1fd51d6390dcbeb18f556
Marius Negruțiu <marius.negrutiu@protonmail.com>
2024-04-05 20:42:53 +0300

    Modified build order to compile mingw-release first

--- _Build_ALL.bat
+++ _Build_ALL.bat
@@ -1,13 +1,13 @@
 @if not exist "%~dp0\curl-ca-bunddle.crt" call "%~dp0\_Get_ca_bundle.bat"
 
-@start "" /WAIT /B %COMSPEC% /C set BUILDER=MSVC^&^&  set CONFIG=Debug^&^&   "%~dp0\_Build.bat"
-@timeout /T 60
-
-@start "" /WAIT /B %COMSPEC% /C set BUILDER=mingw^&^& set CONFIG=Debug^&^&   "%~dp0\_Build.bat"
+@start "" /WAIT /B %COMSPEC% /C set BUILDER=mingw^&^& set CONFIG=Release^&^& "%~dp0\_Build.bat"
 @timeout /T 60
 
 @start "" /WAIT /B %COMSPEC% /C set BUILDER=MSVC^&^&  set CONFIG=Release^&^& "%~dp0\_Build.bat"
 @timeout /T 60
 
-@start "" /WAIT /B %COMSPEC% /C set BUILDER=mingw^&^& set CONFIG=Release^&^& "%~dp0\_Build.bat"
+@start "" /WAIT /B %COMSPEC% /C set BUILDER=mingw^&^& set CONFIG=Debug^&^&   "%~dp0\_Build.bat"
+@timeout /T 60
+
+@start "" /WAIT /B %COMSPEC% /C set BUILDER=MSVC^&^&  set CONFIG=Debug^&^&   "%~dp0\_Build.bat"
 pause
